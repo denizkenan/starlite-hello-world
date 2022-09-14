b63b8bda00b5bdfa38e496e5341d92f5db5310e3
Kenan Deniz <kenandnz@outlook.com>
2022-09-14 15:58:27 +0000

    reproduce bug

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """Minimal Starlite application."""
 from typing import Any
-
+from starlite.datastructures import ResponseHeader
 from starlite import Starlite, get
 
 
-@get("/")
+@get("/", response_headers = {
+        "testheader": ResponseHeader(value="test value",description="test")
+    })
 def hello_world() -> dict[str, Any]:
     """Hi."""
     return {"hello": "world"}
